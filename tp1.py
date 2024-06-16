cc90ba2ec2028771ebaa52b62f54be4facb8abcb
Jean <bj891902@ens.uqam.ca>
2024-06-16 06:00:17 -0400

    comparaison des csv et de la db

--- tp1.py
+++ tp1.py
@@ -1,12 +1,23 @@
 import csv
 import pandas as pd
+import sqlite3
+
 
 def loadData(file):
     return pd.read_csv(file, header=0)
 
-dataset_api_results = loadData('lovoo_v3_users_api-results.csv')
-dataset_instances = loadData('lovoo_v3_users_instances.csv')
+api_df_csv = loadData('lovoo_v3_users_api-results.csv')
+object_df_csv = loadData('lovoo_v3_users_instances.csv')
+csv_merged = pd.merge(api_df_csv, object_df_csv, on="userId") 
+sorted_csv_merged = csv_merged.sort_index(inplace=True)
 
-df = pd.merge(dataset_api_results, dataset_instances, on="userId") 
-print(df) 
-        
+dbfile = 'users_lovoo_v3.db'
+con = sqlite3.connect(dbfile)
+cur = con.cursor()
+object_df_table = pd.read_sql_query("SELECT * FROM objects", con)
+api_df_table = pd.read_sql_query("SELECT * FROM api", con)
+db_merged = pd.merge(object_df_table, api_df_table, on="userId") 
+sorted_db_merged = db_merged.sort_index(inplace=True)
+con.close()
+
+print(sorted_csv_merged == sorted_db_merged)
