54fddaf7f891a84e0bd4861ecfb62246dbed4cbb
seye.ama <seye.ama@courrier.uqam.ca>
2024-07-30 17:49:39 -0400

    Model simple suivant exemple dans tensorflow avant l'Ajout de transfo

--- tp2/tp2.py
+++ tp2/tp2.py
@@ -1,14 +1,88 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
 import numpy as np
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
 import pathlib
 
 
-import pathlib
-data_dir = pathlib.Path("../TP2-images/")
-class_names =['cane', 'cavallo', 'elefante', 'farfalla', 'Fursuit', 'gallina', 'gatto', 'mucca', 'pecora', 'ragno', 'scoiattolo']
-image_count = len(list(data_dir.glob('*/*.jpg')))
+
+data_dir = pathlib.Path(__file__).parent / 'TP2-images'
+
+dataset= tf.data.Dataset.list_files(str(data_dir/'*/*'))
+
+image_count = len(list(data_dir.glob('*/*')))
+batch_size = 1100
+img_height = 64
+img_width = 64
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+
+
 print(image_count)
 
+
 print(tf.__version__)
-print(class_names)
+
+
+
+num_classes = len(class_names)
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(num_classes)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+
+model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=5
+)
+
+
+print(model.summary())
